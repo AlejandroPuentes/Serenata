dbd8c96e4395956a7f26ed7f3fe2cf36bac68d38
Alejandro Puentes Camargo <brianpc200@gmail.com>
2020-05-15 20:00:25 -0500

    Actualizacion

--- Banda/Conexion.py
+++ Banda/Conexion.py
@@ -5,13 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('LLamarM.html')
+
+@app.route('/construir/')
+def LLamarM():
+    banda = Banda()
+    x = banda.cantidad()
+    return render_template('index.html',musicos=x)
+
+@app.route('/construir/prepara/')
+def Preparar():
     banda = Banda()
     banda.construirBanda()
     x = banda.cantidad()
     y = banda.getInstrumentos()
-    return render_template('index.html',musicos=x, instrumento=y)
-
-
+    return render_template('Alistar.html',musicos=x, instrumento=y)
 
 if __name__ == '__main__':
     app.run(debug=True)
