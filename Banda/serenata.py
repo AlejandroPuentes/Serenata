5112c770a7d43c7ab9ed79d030701a1712702394
Johnatan <jgruizba@gmail.com>
2020-05-16 19:51:50 -0500

    Cambios de estilo

--- Banda/serenata.py
+++ Banda/serenata.py
@@ -1,18 +1,35 @@
+from flask import Flask, render_template, url_for, redirect
 from Banda import Banda
+
+app = Flask(__name__)
 
 
 banda = Banda()
-banda.imprimir()
-banda.construirBanda()
-print("Que quiere hacer con la banda ")
-print("1.Afine")
-print("2.Empiece a tocar")
-number=input()
-if (number==1):
-    print("LA BANDA EMPEZO A AFINAR")    
-    banda.prepararSerenata()
-else:
-    print("LA BANDA EMPEZO A TOCAR")
-    banda.tocarSerenata()
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/construir/')
+def home():
+    banda.construirBanda()
+    return render_template('construir.html', musicos=banda.getCantidad())
+
+
+@app.route('/construir/preparar/')
+def preparar():
+    return render_template('alistar.html', instrumentos=banda.getInstrumentos())
+
+@app.route('/construir/tocar/')
+def tocarSerenata():
+    return render_template('tocar.html', instrumentos=banda.getInstrumentos())
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
